5205292317e6b364af6f20836e7b58e9b278b8e4
imshiva17 <shivarajak277@gmail.com>
2026-05-01 19:37:41 +0530

    Created isAuth Middleware and GetCurrentUser API

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./src/config/connectDB.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
-import cors from "cors"
-import authRouter from "./src/routes/auth.route.js"
+import cors from "cors";
+import authRouter from "./src/routes/auth.route.js";
+import userRouter from "./src/routes/user.route.js";
 
 const app = express();
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth",authRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
