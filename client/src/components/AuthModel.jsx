91bdb4c99022e3eb6a117cc59434e258babc9bc0
imshiva17 <shivarajak277@gmail.com>
2026-05-02 19:34:43 +0530

    Working on AuthModel

---
+++ client/src/components/AuthModel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const AuthModel = ({ onclose }) => {
+  const { userData } = useSelector((state) => state.user);
+  return (
+    <div className="fixed inset-0 z-[999] items-center justify-center bg-black/10 backdrop-blur-sm px-4 "></div>
+  );
+};
+
+export default AuthModel;
