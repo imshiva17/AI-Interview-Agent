ddd5987263dd439b60459cacde403865fdcbc098
imshiva17 <shivarajak277@gmail.com>
2026-05-03 22:26:12 +0530

    Working on AuthModel file

--- client/src/components/AuthModel.jsx
+++ client/src/components/AuthModel.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 const AuthModel = ({ onclose }) => {
   const { userData } = useSelector((state) => state.user);
   return (
-    <div className="fixed inset-0 z-[999] items-center justify-center bg-black/10 backdrop-blur-sm px-4 "></div>
+    <div className="fixed inset-0 z-999 items-center justify-center bg-black/10 backdrop-blur-sm px-4 "></div>
   );
 };
 
