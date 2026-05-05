86149d06327713981992c925deceb80d2da14dcc
imshiva17 <shivarajak277@gmail.com>
2026-05-05 18:38:40 +0530

    Home Page Completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -241,33 +241,37 @@ const Home = () => {
                   initial={{ opacity: 0, y: 30 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.5, delay: index * 0.1 }}
-                  whileHover={{ y:-6 }}
+                  whileHover={{ y: -6 }}
                   key={index}
                   className="bg-white border border-gray-200 rounded-3xl p-8 shadow-sm hover:shadow-xl translate-all "
                 >
                   <div className="flex items-center justify-between gap-6 ">
                     <div className="w-1/2">
-                    <h3 className="font-semibold text-xl mb-3  ">{item.title}</h3>
-                    <p className="text-gray-500 text-sm leading-relaxed ">{item.desc}</p>
+                      <h3 className="font-semibold text-xl mb-3  ">
+                        {item.title}
+                      </h3>
+                      <p className="text-gray-500 text-sm leading-relaxed ">
+                        {item.desc}
+                      </p>
                     </div>
                     <div className="w-1/2 flex justify-end ">
-                      <img src={item.image} alt={item.title}
-                      className="w-28 h-28 object-contain "
-                       />
+                      <img
+                        src={item.image}
+                        alt={item.title}
+                        className="w-28 h-28 object-contain "
+                      />
                     </div>
                   </div>
                 </motion.div>
               ))}
             </div>
           </div>
-
-
         </div>
       </div>
 
       {showAuth && <AuthModel onClose={() => setShowAuth(false)} />}
 
-        <Footer/>
+      <Footer />
     </div>
   );
 };
