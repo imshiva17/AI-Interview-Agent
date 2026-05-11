1117e33b1f237a812998b61a46450d4280f5307c
imshiva17 <shivarajak277@gmail.com>
2026-05-11 21:32:49 +0530

    fixed spaced

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -1,35 +1,28 @@
-import React from 'react'
-import maleVideo from "../assets/videos/male-ai.mp4"
-import femaleVideo from "../assets/videos/female-ai.mp4"
+import React from "react";
+import maleVideo from "../assets/videos/male-ai.mp4";
+import femaleVideo from "../assets/videos/female-ai.mp4";
 
-const Step2Interview = ({interviewData, onFinish}) => {
+const Step2Interview = ({ interviewData, onFinish }) => {
+  const { interviewId, questions, userName } = interviewData;
 
-  const{interviewId, questions, userName} = interviewData;
-
-   
   return (
-    <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6 '>
-      <div className='w-full max-w-350 min-h-[80vh] bg-white rounded-3xl shadow-2xl border border-gray-200 flex flex-col lg:flex-row overflow-hidden '>
-
+    <div className="min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6 ">
+      <div className="w-full max-w-350 min-h-[80vh] bg-white rounded-3xl shadow-2xl border border-gray-200 flex flex-col lg:flex-row overflow-hidden ">
         {/* video section */}
-        <div className='w-full lg:w-[35%] bg-white flex flex-col items-center p-6 space-y-6 border-r border-gray-200 '>
-          <div className='w-full max-w-md rounded-2xl overflow-hidden shadow-xl '>
+        <div className="w-full lg:w-[35%] bg-white flex flex-col items-center p-6 space-y-6 border-r border-gray-200 ">
+          <div className="w-full max-w-md rounded-2xl overflow-hidden shadow-xl ">
             <video
-             src={femaleVideo} 
-             muted
-             playsInline
-             preload='auto'
-             className='w-full h-auto object-cover'
-             />
+              src={femaleVideo}
+              muted
+              playsInline
+              preload="auto"
+              className="w-full h-auto object-cover"
+            />
           </div>
-
         </div>
-
-
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Step2Interview
+export default Step2Interview;
