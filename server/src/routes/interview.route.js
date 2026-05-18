7d0cdabcb6015a6388235b638f808369ab7b4778
imshiva17 <shivarajak277@gmail.com>
2026-05-18 21:53:37 +0530

    Created history and report controllers

--- server/src/routes/interview.route.js
+++ server/src/routes/interview.route.js
@@ -5,6 +5,8 @@ import {
   analyzeResume,
   finishInterview,
   generateQuestion,
+  getInterviewReport,
+  getMyInterviews,
   submitAnswer,
 } from "../controllers/interview.controller.js";
 
@@ -14,6 +16,7 @@ interviewRouter.post("/resume", isAuth, upload.single("resume"), analyzeResume);
 interviewRouter.post("/generate-questions", isAuth, generateQuestion);
 interviewRouter.post("/submit-answer", isAuth, submitAnswer);
 interviewRouter.post("/finish", isAuth, finishInterview);
-
+interviewRouter.get("/get-interview", isAuth, getMyInterviews);
+interviewRouter.get("/report/:id", isAuth, getInterviewReport);
 
 export default interviewRouter;
