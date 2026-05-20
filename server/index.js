bcb1240e80067846f1c743065022fd1a456ae539
imshiva17 <shivarajak277@gmail.com>
2026-05-20 21:42:46 +0530

    Razorpay Setup completed

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import authRouter from "./src/routes/auth.route.js";
 import userRouter from "./src/routes/user.route.js";
 import interviewRouter from "./src/routes/interview.route.js";
+import paymentRouter from "./src/routes/payment.route.js";
 
 const app = express();
 app.use(
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/interview", interviewRouter);
+app.use("/api/payment", paymentRouter);
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
